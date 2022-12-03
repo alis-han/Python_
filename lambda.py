9bb25ccb2776ea12b324b438e213a271937779c0
Alishan <mdsalishan999@gmail.com>
2022-12-03 16:37:12 +0530

    Add

---
+++ lambda.py
@@ -0,0 +1,14 @@
+list1=[3,4,10,9,18,66,13,15]
+# def isEven(i):
+#     return i%2==0
+
+evenNum=list(filter(lambda i: i>5,list1))
+print(evenNum)
+
+cubeNum=list(map(lambda i:i**3,list1))
+print(cubeNum)
+
+# To swap lowerCase to UpperCase and vice-versa...
+list2=["a","B","c","D","e","f"]
+alpha=list(map(lambda i:i.swapcase(),list2))
+print(alpha)
